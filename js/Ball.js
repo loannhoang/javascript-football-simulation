18fa6905277f3388f4ca3f782f8eec83306131ed
Garrett Foster <garrett.d.foster@gmail.com>
2018-11-05 21:38:26 -0600

    resized ball back to normal size

--- js/Ball.js
+++ js/Ball.js
@@ -3,7 +3,7 @@ class Ball {
         this.scene = scene;
 
         //create ball sprite at given location
-        var sprite = scene.physics.add.image(x, y, 'football').setDisplaySize(1.5*10/3/3*this.scene.px_per_yd,1.5*7/3/3*this.scene.px_per_yd);
+        var sprite = scene.physics.add.image(x, y, 'football').setDisplaySize(11/3/3*this.scene.px_per_yd,1.5*8/3/3*this.scene.px_per_yd);
 
         //create pointer to physics body
         this.body = sprite.body;
@@ -11,5 +11,13 @@ class Ball {
         //set a few physics options for the ball
         this.body.setCollideWorldBounds(true)
             .setMass(1/32.2);
+    }
+
+    moveTo(){
+
+    }
+
+    moveWith(){
+        
     }
 }
